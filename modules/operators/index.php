38f78fab4ab05efa5cfe1d6d0e00db1db9a36d92
simone-losito <simone.losito@gmail.com>
2026-04-28 21:52:47 +0200

    Add op_badges.php to operators module

--- modules/operators/index.php
+++ modules/operators/index.php
@@ -3,6 +3,7 @@
 
 require_once __DIR__ . '/../../core/helpers.php';
 require_once **DIR** . '/components/op_base.php';
+require_once __DIR__ . '/components/op_badges.php';
 
 require_login();
 require_permission('operators.view');
