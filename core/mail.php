83d9e2c4cb333c514b914105771866a57a3f7a24
simone-losito <simone.losito@gmail.com>
2026-04-29 20:10:25 +0200

    Make mail helper safe when PHPMailer is missing

--- core/mail.php
+++ core/mail.php
@@ -1,18 +1,35 @@
 <?php
 // core/mail.php
-// Invio email con PHPMailer (SMTP reale)
+// Invio email SMTP con PHPMailer se disponibile.
+// Se vendor/PHPMailer non è presente, il software non va in fatal error.
 
-require_once __DIR__ . '/../vendor/PHPMailer/src/PHPMailer.php';
-require_once __DIR__ . '/../vendor/PHPMailer/src/SMTP.php';
-require_once __DIR__ . '/../vendor/PHPMailer/src/Exception.php';
 require_once __DIR__ . '/settings.php';
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+$phpMailerBase = __DIR__ . '/../vendor/PHPMailer/src/';
+$phpMailerAvailable = is_file($phpMailerBase . 'PHPMailer.php')
+    && is_file($phpMailerBase . 'SMTP.php')
+    && is_file($phpMailerBase . 'Exception.php');
+
+if ($phpMailerAvailable) {
+    require_once $phpMailerBase . 'PHPMailer.php';
+    require_once $phpMailerBase . 'SMTP.php';
+    require_once $phpMailerBase . 'Exception.php';
+}
+
+if (!function_exists('mail_phpmailer_available')) {
+    function mail_phpmailer_available(): bool
+    {
+        return class_exists('PHPMailer\\PHPMailer\\PHPMailer');
+    }
+}
 
 if (!function_exists('send_email')) {
     function send_email(string $to, string $subject, string $html, string $alt = '', array $attachments = []): bool
     {
+        if (!mail_phpmailer_available()) {
+            return false;
+        }
+
         $to = trim($to);
         $subject = trim($subject);
         $html = (string)$html;
@@ -24,17 +41,17 @@ if (!function_exists('send_email')) {
 
         $host = trim((string)setting('smtp_host', ''));
         $port = (int)setting('smtp_port', 587);
-        $username = trim((string)setting('smtp_username', ''));
-        $password = (string)setting('smtp_password', '');
-        $encryption = trim((string)setting('smtp_encryption', 'tls'));
-        $fromAddress = trim((string)setting('mail_from_address', ''));
-        $fromName = trim((string)setting('mail_from_name', 'Turnar'));
+        $username = trim((string)setting('smtp_user', setting('smtp_username', '')));
+        $password = (string)setting('smtp_pass', setting('smtp_password', ''));
+        $encryption = trim((string)setting('smtp_secure', setting('smtp_encryption', 'tls')));
+        $fromAddress = trim((string)setting('email_from', setting('mail_from_address', '')));
+        $fromName = trim((string)setting('email_from_name', setting('mail_from_name', 'Turnar')));
 
         if ($host === '' || $fromAddress === '') {
             return false;
         }
 
-        $mail = new PHPMailer(true);
+        $mail = new PHPMailer\PHPMailer\PHPMailer(true);
 
         try {
             $mail->CharSet = 'UTF-8';
@@ -51,9 +68,9 @@ if (!function_exists('send_email')) {
             }
 
             if ($encryption === 'ssl') {
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS;
             } elseif ($encryption === 'tls') {
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
             } else {
                 $mail->SMTPSecure = false;
                 $mail->SMTPAutoTLS = false;
@@ -62,7 +79,7 @@ if (!function_exists('send_email')) {
             $mail->setFrom($fromAddress, $fromName !== '' ? $fromName : 'Turnar');
             $mail->addAddress($to);
 
-            $replyTo = trim((string)setting('mail_reply_to', ''));
+            $replyTo = trim((string)setting('email_reply_to', setting('mail_reply_to', '')));
             if ($replyTo !== '') {
                 $mail->addReplyTo($replyTo);
             }
@@ -81,8 +98,6 @@ if (!function_exists('send_email')) {
             $mail->AltBody = $alt !== '' ? $alt : trim(strip_tags(str_replace(['<br>', '<br/>', '<br />'], "\n", $html)));
 
             return $mail->send();
-        } catch (Exception $e) {
-            return false;
         } catch (Throwable $e) {
             return false;
         }
