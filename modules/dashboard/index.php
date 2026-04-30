6b2bc9f09402e13f2b6ebb283ba0eb5c2672dcd2
simone-losito <simone.losito@gmail.com>
2026-04-30 18:13:59 +0200

    Protect dashboard module redirect

--- modules/dashboard/index.php
+++ modules/dashboard/index.php
@@ -1,7 +1,11 @@
 <?php
+// modules/dashboard/index.php
+// Redirect dashboard protetto da modulo e permessi
+
 require_once __DIR__ . '/../../core/settings.php';
 
 require_login();
+require_module('dashboard');
 require_permission('dashboard.view');
 
 redirect('');
